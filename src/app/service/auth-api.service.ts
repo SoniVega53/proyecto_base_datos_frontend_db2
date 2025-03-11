98d06100a11e4aef4269e0c17466b0266d5d5fc1
SoniV53 <sonivega53@gmail.com>
2025-03-10 22:19:04 -0600

    inicio

---
+++ src/app/service/auth-api.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import { BaseApiService } from './base-api.service';
+import { UserRequest } from '../entity/UserRequest';
+import { Observable } from 'rxjs';
+import { jwtDecode } from "jwt-decode";
+import { UserEntity } from '../entity/UserEntity';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthApiService extends BaseApiService {
+
+  private tokenKey = 'token';
+
+
+  login(body: UserRequest): Observable<any> {
+    return this.postToken(`noauth/login`, body);
+  }
+
+  register(body: UserEntity): Observable<any> {
+    return this.postToken(`noauth/register`, body);
+  }
+
+  logout() {
+    localStorage.removeItem(this.tokenKey);
+    this.router.navigate(['/login']);
+  }
+
+  isAuthenticated(): boolean {
+    if (this.isLocalStorageAvailable()){
+      if (this.isTokenExpired()){
+        this.logout()
+        return false
+      }
+      return !!localStorage.getItem('token')
+    };
+    return false;
+  }
+
+  isTokenExpired(): boolean {
+    const token = this.getToken();
+    if (token == null) return false;
+
+    const decodedToken = this.getDecodedToken();
+    if (decodedToken && decodedToken.exp) {
+      const expirationDate = new Date(decodedToken.exp * 1000);
+      return expirationDate < new Date()
+    }
+    return false;
+  }
+
+
+  getToken(): string | null {
+    if(typeof window !== 'undefined'){
+      return localStorage.getItem(this.tokenKey);
+    }else {
+      return null;
+    }
+  }
+
+  getDecodedToken(): any {
+    const token = this.getToken();
+    if (token) {
+      return jwtDecode(token);
+    }
+    return null;
+  }
+
+  getUserRole(): string | null {
+    const decodedToken = this.getDecodedToken();
+    if (decodedToken && decodedToken.roles) {
+      return decodedToken.roles;
+    }
+    return null;
+  }
+
+  getUserData() {
+    const decodedToken = this.getDecodedToken();
+    return decodedToken;
+  }
+
+
+  public isLocalStorageAvailable(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
+  }
+}
---
+++ src/app/service/user-api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserEntity } from '../entity/UserEntity';
+import { AuthApiService } from './auth-api.service';
+import { UserEntityRequest } from '../entity/UserEntityRequest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserApiService extends AuthApiService{
+
+
+  getAllUsers(): Observable<UserEntity[]>{
+    return this.getService("admin/user/see");
+  }
+  getInfoUser(): Observable<any>{
+    const token = this.getToken()
+    return this.postServiceBody(`user/usuario?token=${token}`,null);
+  }
+
+  deleteUsuario(idUsuario:Number): Observable<any>{
+    return this.deleteService(`user/usuario/eliminar/${idUsuario}`);
+  }
+
+  updateUserPassword(body:UserEntityRequest,idUser:Number):Observable<any>{
+    return this.postServiceBody(`user/usuario/update/password/${idUser}`,body);
+  }
+
+  updateUser(body:UserEntityRequest,idUser:Number):Observable<any>{
+    return this.postServiceBody(`user/usuario/update/${idUser}`,body);
+  }
+
+
+}
